24b2239e7c1ecf8e0e91f38474101e42e20432b9
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 07:29:54 +0300

    finish

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>0 Баллов</h1>
+      <p>
+        Ягоды, фасоль, натуральные овощные соки.
+      </p> 
+    </div>
+  );
+}
+
+export default App;
