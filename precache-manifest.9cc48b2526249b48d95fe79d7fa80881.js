f438fc7a933a16868af38a110fe24a2987bac84e
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 07:30:27 +0300

    Updates

---
+++ precache-manifest.9cc48b2526249b48d95fe79d7fa80881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357d8b11659ee040e011202104c84684",
+    "url": "/one-point/index.html"
+  },
+  {
+    "revision": "1947f71b4f0ba8b4c77e",
+    "url": "/one-point/static/css/main.7157ab00.chunk.css"
+  },
+  {
+    "revision": "ba653f549180d49b05ef",
+    "url": "/one-point/static/js/2.45ccdb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/one-point/static/js/2.45ccdb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1947f71b4f0ba8b4c77e",
+    "url": "/one-point/static/js/main.c2f19973.chunk.js"
+  },
+  {
+    "revision": "2b2c3cc0a7661d375d25",
+    "url": "/one-point/static/js/runtime-main.f908b679.js"
+  }
+]);
