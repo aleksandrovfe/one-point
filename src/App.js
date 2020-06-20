9e20d471b1c05f96036d75581fe4ecd03108045a
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 07:30:39 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>0 Баллов</h1>
+      <h1>1 Балл</h1>
       <p>
         Ягоды, фасоль, натуральные овощные соки.
       </p> 
